af98d90710bda11b17327849c156f39ac045f5ea
Janis Stals <stalsjanis@gmail.com>
2023-06-19 22:10:02 +0300

    initial functions

--- main.go
+++ main.go
@@ -1,5 +1,30 @@
+// Ebay Quantites sync
+// By Janis Stals aka jssdeveloper 2023 under MIT license
+// For further information please read LICENSE file
+
 package main
 
+func csv() {
+
+}
+
+func sqlite() {
+
+}
+
+func postgres() {
+
+}
+
 func main() {
+
+	// Use this if you would like to read quantities from csv file
+	// csv()
+
+	// Use this if you would like to read quantities from sqlite database
+	// sqlite()
+
+	// Use this if you would like to read quantities from postgres database
+	// postgres()
 
 }
